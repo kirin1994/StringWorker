af82fe214649a90fcf40cf941fec6d1fbfe59cba
Adrian Podlawski <apodlawski@gmail.com>
2020-10-25 23:52:10 +0100

    Cleanup solution, blazor Action list component setup

--- Infrastructure2/Plugins/PluginsFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Infrastructure.Plugins
-{
-    public class PluginsFactory
-    {
-    }
-}
--- Infrastructure3/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Infrastructure3
-{
-    public class Class1
-    {
-    }
-}
