ba23a1c7a46abc6b3600088c1b12e47254bc6911
Adrian Podlawski <apodlawski@gmail.com>
2020-10-26 13:39:50 +0100

    Fix connection string for web api

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,7 +29,7 @@ namespace WebAPI
         {
             services.AddControllers();
             services.AddScoped<IActionAsyncRepository, ActionAsyncRepository>();
-            services.AddDbContext<ActionDbContext>();
+            services.AddDbContext<ActionDbContext>(options => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=StringWorker;Integrated Security=True"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
